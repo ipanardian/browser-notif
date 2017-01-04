0ad1c318adb6540cc598a76ed69f4d978e7a1266
ipanardian <ipan.software@gmail.com>
2017-01-04 15:24:56 +0700

    ServiceWorkerApi script

---
+++ src/sw.ts
@@ -0,0 +1,35 @@
+/**
+*  BrowserNotif.JS
+*  (c) 2016 Ipan Ardian
+*
+*  Lets a web page send notifications that are displayed outside the page at the system level. 
+*  This lets web apps send information to a user even if the application is idle, in the background, switched tabs or moved to a different app.
+*  For details, see the web site: https://github.com/ipanardian/browser-notif
+*  The MIT License
+*
+*  This is ServiceWorkerApi script
+*/
+
+console.log('Started');
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed');
+});
+self.addEventListener('activate', function(event) {
+  console.log('Activated');
+});
+self.onnotificationclick = function(event) {
+    if (typeof event.notification.data != 'null') {
+      try {
+        var data = JSON.parse(event.notification.data)
+      } catch (error) {
+        console.error('BrowserNotif: Error parse '+ error)
+      }
+      try {
+        Function("(" +data['clickOnServiceWorker']+ ")()")()   
+      } catch (error) {
+        console.error('BrowserNotif: Error clickOnServiceWorker '+ error)
+      }
+    }
+    event.notification.close()
+};
